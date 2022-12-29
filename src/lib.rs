d72a2488cb022ba6a27b59c11adeef45560b5f54
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2022-12-29 14:05:27 +0100

    make circle follow mouse

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,11 @@
 
 mod palette;
 
-use bevy::{core_pipeline::clear_color::ClearColorConfig, prelude::*, render::camera::ScalingMode};
+use bevy::{
+    core_pipeline::clear_color::ClearColorConfig,
+    prelude::*,
+    render::camera::{RenderTarget, ScalingMode},
+};
 use bevy_prototype_lyon::prelude::*;
 
 const VIEWPORT_SIZE: f32 = 20.0;
@@ -11,9 +15,18 @@ pub struct AppPlugin;
 
 impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup);
+        app.init_resource::<CursorPosition>()
+            .add_startup_system(setup)
+            .add_system(update_cursor_positon)
+            .add_system(follow.after(update_cursor_positon));
     }
 }
+
+#[derive(Resource, Default, Deref, DerefMut)]
+struct CursorPosition(Option<Vec2>);
+
+#[derive(Component)]
+struct Follow;
 
 fn setup(mut commands: Commands) {
     // camera
@@ -34,9 +47,42 @@ fn setup(mut commands: Commands) {
         sides: 20,
         ..default()
     };
-    commands.spawn(GeometryBuilder::build_as(
-        &shape,
-        DrawMode::Fill(FillMode::color(palette::LIGHT_WHITE)),
-        Transform::default(),
+    commands.spawn((
+        GeometryBuilder::build_as(
+            &shape,
+            DrawMode::Fill(FillMode::color(palette::LIGHT_WHITE)),
+            Transform::default(),
+        ),
+        Follow,
     ));
+}
+
+fn update_cursor_positon(
+    windows: Res<Windows>,
+    query: Query<(&Camera, &GlobalTransform)>,
+    mut cursor_position: ResMut<CursorPosition>,
+) {
+    let (camera, transform) = query.single();
+    let window = match camera.target {
+        RenderTarget::Window(id) => windows.get(id).unwrap(),
+        RenderTarget::Image(_) => panic!(),
+    };
+    **cursor_position = window.cursor_position().map(|screen_position| {
+        let size = Vec2::new(window.width() as f32, window.height() as f32);
+        // convert screen position [0..resolution] to ndc [-1..1] (gpu coordinates)
+        let ndc = (screen_position / size) * 2.0 - Vec2::ONE;
+        // matrix for undoing the projection and camera transform
+        let ndc_to_world = transform.compute_matrix() * camera.projection_matrix().inverse();
+        // use it to convert ndc to world-space coordinates
+        ndc_to_world.project_point3(ndc.extend(-1.0)).truncate()
+    });
+}
+
+fn follow(mut query: Query<&mut Transform, With<Follow>>, cursor_position: Res<CursorPosition>) {
+    for mut transform in &mut query {
+        if let Some(cursor_position) = **cursor_position {
+            transform.translation.x = cursor_position.x;
+            transform.translation.y = cursor_position.y;
+        }
+    }
 }
