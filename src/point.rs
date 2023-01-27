33ebb3596ce7fde1d9a78dbec06d60af39ce3cdd
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2023-01-27 19:16:37 +0100

    use diamonds for point representation

--- src/point.rs
+++ src/point.rs
@@ -1,9 +1,11 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
 
 use crate::{palette, tool::Selected};
 
-pub const POINT_RADIUS: f32 = 5.0;
+pub const POINT_RADIUS: f32 = 0.1;
 
 #[derive(SystemLabel)]
 pub struct PointUpdate;
@@ -36,8 +38,8 @@ fn spawn_points(
     mut commands: Commands,
 ) {
     for event in events.iter() {
-        let shape = shapes::Circle {
-            radius: POINT_RADIUS,
+        let shape = shapes::Rectangle {
+            extents: Vec2::splat(POINT_RADIUS),
             ..default()
         };
         let entity = commands
@@ -45,7 +47,11 @@ fn spawn_points(
                 GeometryBuilder::build_as(
                     &shape,
                     DrawMode::Fill(FillMode::color(palette::LIGHT_WHITE)),
-                    Transform::from_translation(event.position.extend(0.0)),
+                    Transform {
+                        translation: event.position.extend(0.0),
+                        rotation: Quat::from_rotation_z(PI / 4.0),
+                        ..default()
+                    },
                 ),
                 Point,
             ))
