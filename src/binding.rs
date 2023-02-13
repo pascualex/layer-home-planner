61a98457fe9020ce39174f0e2b02b148fe376ed2
Alejandro Pascual Pozo <alejandro.pascual.pozo@gmail.com>
2023-02-13 10:13:06 +0100

    move bind functions to binding structs

--- src/binding.rs
+++ src/binding.rs
@@ -18,6 +18,98 @@ impl Plugin for BindingPlugin {
     }
 }
 
+struct DefaultBindings;
+
+impl DefaultBindings {
+    fn bind(
+        hover: &Hover,
+        mouse_input: &Input<MouseButton>,
+        keyboard_input: &Input<KeyCode>,
+    ) -> BindedAction {
+        if keyboard_input.just_pressed(KeyCode::E) {
+            BindedAction::Create
+        } else if let Some(hover) = hover.point {
+            if mouse_input.just_pressed(MouseButton::Left) {
+                BindedAction::Select(hover)
+            } else {
+                BindedAction::None
+            }
+        } else {
+            BindedAction::None
+        }
+    }
+}
+
+struct SelectBindings;
+
+impl SelectBindings {
+    #[allow(clippy::collapsible_if)]
+    #[allow(clippy::collapsible_else_if)]
+    fn bind(
+        selection: Entity,
+        hover: &Hover,
+        mouse_input: &Input<MouseButton>,
+        keyboard_input: &Input<KeyCode>,
+    ) -> BindedAction {
+        if keyboard_input.just_pressed(KeyCode::G) {
+            BindedAction::Track(selection)
+        } else if keyboard_input.just_pressed(KeyCode::E) {
+            BindedAction::Extend(selection)
+        } else if keyboard_input.just_pressed(KeyCode::Delete) {
+            BindedAction::Delete(selection)
+        } else if keyboard_input.just_pressed(KeyCode::Escape) {
+            BindedAction::Unselect
+        } else if let Some(hover) = hover.point {
+            if mouse_input.just_pressed(MouseButton::Left) && hover != selection {
+                BindedAction::Select(hover)
+            } else {
+                BindedAction::None
+            }
+        } else {
+            if mouse_input.just_pressed(MouseButton::Left) {
+                BindedAction::Unselect
+            } else {
+                BindedAction::None
+            }
+        }
+    }
+}
+
+struct TrackBindings;
+
+impl TrackBindings {
+    #[allow(clippy::collapsible_if)]
+    #[allow(clippy::collapsible_else_if)]
+    fn bind(
+        selection: Entity,
+        mode: TrackMode,
+        hover: &Hover,
+        mouse_input: &Input<MouseButton>,
+        keyboard_input: &Input<KeyCode>,
+    ) -> BindedAction {
+        if keyboard_input.just_pressed(KeyCode::Delete) {
+            BindedAction::Delete(selection)
+        } else if keyboard_input.just_pressed(KeyCode::Escape) {
+            match mode {
+                TrackMode::Move(old_position) => BindedAction::Move(selection, old_position),
+                TrackMode::Place => BindedAction::Delete(selection),
+            }
+        } else if let Some(hover) = hover.point {
+            if mouse_input.just_pressed(MouseButton::Left) {
+                BindedAction::Merge(selection, hover)
+            } else {
+                BindedAction::None
+            }
+        } else {
+            if mouse_input.just_pressed(MouseButton::Left) {
+                BindedAction::Select(selection)
+            } else {
+                BindedAction::None
+            }
+        }
+    }
+}
+
 fn process_bindings(
     plan_mode: Res<PlanMode>,
     hover: Res<Hover>,
@@ -25,59 +117,13 @@ fn process_bindings(
     keyboard_input: Res<Input<KeyCode>>,
     mut binded_action: ResMut<BindedAction>,
 ) {
-    *binded_action = BindedAction::None;
-    #[allow(clippy::collapsible_if)]
-    #[allow(clippy::collapsible_else_if)]
-    match *plan_mode {
-        PlanMode::Default => {
-            if keyboard_input.just_pressed(KeyCode::E) {
-                *binded_action = BindedAction::Create;
-            } else if let Some(hover) = hover.point {
-                if mouse_input.just_pressed(MouseButton::Left) {
-                    *binded_action = BindedAction::Select(hover);
-                }
-            }
-        }
+    *binded_action = match *plan_mode {
+        PlanMode::Default => DefaultBindings::bind(&hover, &mouse_input, &keyboard_input),
         PlanMode::Select(selection) => {
-            if keyboard_input.just_pressed(KeyCode::G) {
-                *binded_action = BindedAction::Track(selection);
-            } else if keyboard_input.just_pressed(KeyCode::E) {
-                *binded_action = BindedAction::Extend(selection);
-            } else if keyboard_input.just_pressed(KeyCode::Delete) {
-                *binded_action = BindedAction::Delete(selection)
-            } else if keyboard_input.just_pressed(KeyCode::Escape) {
-                *binded_action = BindedAction::Unselect;
-            } else if let Some(hover) = hover.point {
-                if mouse_input.just_pressed(MouseButton::Left) && hover != selection {
-                    *binded_action = BindedAction::Select(hover);
-                }
-            } else {
-                if mouse_input.just_pressed(MouseButton::Left) {
-                    *binded_action = BindedAction::Unselect;
-                }
-            }
+            SelectBindings::bind(selection, &hover, &mouse_input, &keyboard_input)
         }
         PlanMode::Track(selection, track_mode) => {
-            if keyboard_input.just_pressed(KeyCode::Delete) {
-                *binded_action = BindedAction::Delete(selection);
-            } else if keyboard_input.just_pressed(KeyCode::Escape) {
-                match track_mode {
-                    TrackMode::Move(old_position) => {
-                        *binded_action = BindedAction::Move(selection, old_position);
-                    }
-                    TrackMode::Place => {
-                        *binded_action = BindedAction::Delete(selection);
-                    }
-                }
-            } else if let Some(hover) = hover.point {
-                if mouse_input.just_pressed(MouseButton::Left) {
-                    *binded_action = BindedAction::Merge(selection, hover);
-                }
-            } else {
-                if mouse_input.just_pressed(MouseButton::Left) {
-                    *binded_action = BindedAction::Select(selection);
-                }
-            }
+            TrackBindings::bind(selection, track_mode, &hover, &mouse_input, &keyboard_input)
         }
     }
 }
